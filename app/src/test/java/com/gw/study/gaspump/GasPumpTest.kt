5aefabc881974c89ec429da6d7468d188bf9755c
Giwung <kiwung0701@gmail.com>
2023-12-18 16:38:41 +0900

    GasPump, GasPumpTests 리팩토링

--- app/src/test/java/com/gw/study/gaspump/GasPumpTest.kt
+++ app/src/test/java/com/gw/study/gaspump/GasPumpTest.kt
@@ -1,43 +1,50 @@
 package com.gw.study.gaspump
 
-import com.gw.study.gaspump.gas.BreadBoard
 import com.gw.study.gaspump.gas.Gas
 import com.gw.study.gaspump.gas.GasPump
-import com.gw.study.gaspump.gas.Process
 import com.gw.study.gaspump.gas.PumpEngine
-import junit.framework.TestCase.assertEquals
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.whenever
 
+@RunWith(MockitoJUnitRunner::class)
 class GasPumpTest {
 
-    @Test
-    fun gasPumpTest() = runTest {
-        val breadBoard = BreadBoard()
+    @Mock
+    private lateinit var engine: PumpEngine
 
-        val pump = GasPump(
-            gas = Gas.Gasoline,
-            engine = PumpEngine(),
-            fProcess = breadBoard.fProcess.combine(breadBoard.fGasType) { process: Process, gas: Gas -> gas to process },
-            cScope = this
-        )
-        var liters = 0
-        launch {
-            pump().collect {
-                liters++
+    private lateinit var gasPump: GasPump
+
+    @Before
+    fun setUp() {
+        val action = flow {
+            emit(Unit)
+        }
+        whenever(engine.invoke()).thenReturn(action)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun whenCollect_shouldReturnGasType() = runTest {
+        gasPump = GasPump(Gas.Gasoline, engine = engine, fProcess = emptyFlow(), this)
+        var actual: Gas? = null
+        val job = launch {
+            gasPump.invoke().collect {
+                actual = it
             }
         }
-
-        breadBoard.process = Process.Start
-        delay(1000L)
-
-        breadBoard.process = Process.Pause
-        delay(1000L)
-
-        assertEquals(21, liters)
-        breadBoard.process = Process.Destroy
+        advanceTimeBy(10)
+        job.cancel()
+        Assert.assertEquals(Gas.Gasoline, actual)
     }
 }

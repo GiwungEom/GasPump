fc8946cd410e00db4d3c9a3c17151a1b2abd7703
Giwung <kiwung0701@gmail.com>
2023-12-19 11:27:06 +0900

    GasPrice, GasPriceTest 리팩토링

--- app/src/main/java/com/gw/study/gaspump/gas/price/GasPrice.kt
+++ app/src/main/java/com/gw/study/gaspump/gas/price/GasPrice.kt
@@ -5,6 +5,7 @@ import com.gw.study.gaspump.gas.price.model.Price
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.runningFold
 
 class GasPrice {
 
@@ -17,5 +18,8 @@ class GasPrice {
     fun calc(fLiter: Flow<Int>, fGas: StateFlow<Gas>): Flow<Int> =
         fLiter.map { it * prices.getValue(fGas.value).pricePerLiter }
 
-
+    fun calcAcc(fuelFlow: Flow<Gas>): Flow<Int> =
+        fuelFlow.runningFold(0) { accumulator, gas ->
+            accumulator + prices.getValue(gas).pricePerLiter
+        }
 }
---
+++ app/src/test/java/com/gw/study/gaspump/gas/price/GasPriceTest.kt
@@ -0,0 +1,77 @@
+package com.gw.study.gaspump.gas.price
+
+import com.gw.study.gaspump.gas.model.Gas
+import com.gw.study.gaspump.gas.price.model.Price
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
+
+class GasPriceTest {
+
+    @Test
+    fun whenInit_shouldBePriceMapEmpty() {
+        val gasPrice = GasPrice()
+        Assert.assertEquals(0, gasPrice.prices.size)
+    }
+
+    @Test
+    fun whenAddPrice_shouldNotBeEmpty() {
+        val gasPrice = GasPrice()
+        val price = Price(Gas.Gasoline, 8)
+        gasPrice.addPrice(price)
+        Assert.assertTrue(gasPrice.prices.isNotEmpty())
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun whenFuelFlowsIn_shouldCalcWithGasPriceCumulatively() = runTest(UnconfinedTestDispatcher()) {
+        val gasPrice = GasPrice()
+        val price = Price(Gas.Gasoline, 8)
+        gasPrice.addPrice(price)
+
+        val expected = listOf(0, 8, 16, 24)
+        val actual = mutableListOf<Int>()
+
+        val fuelFlow = flow {
+            repeat(3) {
+                emit(Gas.Gasoline)
+            }
+        }
+        val priceFlow = gasPrice.calcAcc(
+            fuelFlow
+        )
+        launch {
+            priceFlow.toList(actual)
+        }
+
+        Assert.assertEquals(expected.joinToString(), actual.joinToString())
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun gasPricePerLiterTest() = runTest(UnconfinedTestDispatcher()) {
+        val gasPrice = GasPrice()
+        val price = Price(Gas.Gasoline, 8)
+        gasPrice.addPrice(price)
+        val expected = listOf(0, 8, 16)
+        val actual = mutableListOf<Int>()
+
+        launch {
+            gasPrice.calc(
+                flow {
+                    repeat(3) {
+                        emit(it)
+                    }
+                },
+                MutableStateFlow(Gas.Gasoline)
+            ).toList(actual)
+        }
+        Assert.assertEquals(expected.joinToString(), actual.joinToString())
+    }
+}

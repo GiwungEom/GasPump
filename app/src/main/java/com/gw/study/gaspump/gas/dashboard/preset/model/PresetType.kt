7a25ed361b96dc8b3a4944665a0b8f5ae238bf55
Giwung <kiwung0701@gmail.com>
2023-12-22 11:11:30 +0900

    Test Dashboard Builder 및 테스트 추가

---
+++ app/src/main/java/com/gw/study/gaspump/gas/dashboard/preset/model/PresetType.kt
@@ -0,0 +1,6 @@
+package com.gw.study.gaspump.gas.dashboard.preset.model
+
+enum class PresetType {
+    Payment,
+    Gas
+}
---
+++ app/src/test/java/com/gw/study/gaspump/gas/dashboard/builder/TestDashboardBuilder.kt
@@ -0,0 +1,69 @@
+package com.gw.study.gaspump.gas.dashboard.builder
+
+import com.gw.study.gaspump.gas.dashboard.GasPumpDashboard
+import com.gw.study.gaspump.gas.dashboard.preset.PresetGauge
+import com.gw.study.gaspump.gas.price.GasPrice
+import com.gw.study.gaspump.gas.pump.GasPump
+import com.gw.study.gaspump.gas.state.BreadBoard
+import kotlinx.coroutines.CoroutineScope
+import kotlin.reflect.KClass
+
+typealias Stub = () -> Unit
+
+/**
+ * Dashboard 의존성 객체 및 stub 관리 간소화
+ */
+class TestDashboardBuilder {
+
+    private val stubs: MutableMap<KClass<out Any>, () -> Unit> = mutableMapOf()
+
+    private lateinit var gasPump: GasPump
+    private lateinit var gasPrice: GasPrice
+    private lateinit var engineBreadBoard: BreadBoard
+    private lateinit var presetGauge: PresetGauge
+    private lateinit var scope: CoroutineScope
+
+    fun setGasPump(gasPump: GasPump): TestDashboardBuilder {
+        this.gasPump = gasPump
+        return this
+    }
+
+    fun setGasPrice(gasPrice: GasPrice): TestDashboardBuilder {
+        this.gasPrice = gasPrice
+        return this
+    }
+
+    fun setEngineBreadBoard(engineBreadBoard: BreadBoard): TestDashboardBuilder {
+        this.engineBreadBoard = engineBreadBoard
+        return this
+    }
+
+    fun setPresetGauge(presetGauge: PresetGauge): TestDashboardBuilder {
+        this.presetGauge = presetGauge
+        return this
+    }
+
+    fun setScope(scope: CoroutineScope): TestDashboardBuilder {
+        this.scope = scope
+        return this
+    }
+
+    fun addStubs(vararg stubs: Pair<KClass<out Any>, Stub>): TestDashboardBuilder {
+        this.stubs.putAll(stubs.toMap())
+        return this
+    }
+
+    @Throws(UninitializedPropertyAccessException::class)
+    fun build(): GasPumpDashboard {
+        if (::gasPump.isInitialized &&
+            ::gasPrice.isInitialized &&
+            ::engineBreadBoard.isInitialized &&
+            ::presetGauge.isInitialized &&
+            ::scope.isInitialized) {
+            stubs.forEach { it.value() }
+            return GasPumpDashboard(gasPump, gasPrice, engineBreadBoard, presetGauge, scope)
+        } else {
+            throw UninitializedPropertyAccessException()
+        }
+    }
+}
